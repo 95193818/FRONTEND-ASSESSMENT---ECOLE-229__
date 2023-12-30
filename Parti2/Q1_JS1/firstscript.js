54cc0e4ac81e1d72fdea3e0204101fec03af5fa1
95193818 <mamamzeidou@gmail.com>
2023-12-30 15:53:46 +0100

    DEVOIR

---
+++ Parti2/Q1_JS1/firstscript.js
@@ -0,0 +1,11 @@
+ let firstname = String(prompt("Entrez votre"));
+ let lastName = String(prompt("Entrez votre prénom"));
+ let age = Number(prompt("Entrez anné de naissance"));
+ let result = age - 2023
+ 
+   alert("hello" + firstname + lastName + "vous avez" + result + "ans");
+
+
+
+
+
